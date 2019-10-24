7bc8cec664f2cfd99aeb367ff8c881984fa8972e
Simon Pasquier <spasquie@redhat.com>
2019-10-24 11:33:14 +0200

    Add build_info metric

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from flask import Flask, escape, request, abort
-from prometheus_client import make_wsgi_app, Counter
+from prometheus_client import make_wsgi_app, Counter, Gauge
 from werkzeug.wsgi import DispatcherMiddleware
 
 app = Flask(__name__)
 
 HELLO_COUNTER = Counter('hellos', 'Total number of hellos')
 HELLO_FAILED_COUNTER = Counter('hellos_failed', 'Total number of hellos')
+INFO = Gauge('build_info', 'Information about the application', ['version'])
+INFO.labels('1.0.0').set(1)
 
 @app.route('/')
 def hello():
