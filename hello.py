92105cbf254671f91d374195542dde547ed1bf61
Simon Pasquier <spasquie@redhat.com>
2021-09-20 22:33:39 +0200

    Use counter of HTTP requests per status code

--- hello.py
+++ hello.py
@@ -7,22 +7,24 @@ app = Flask(__name__)
 INFO = Gauge('build_info', 'Information about the application', ['version'])
 INFO.labels('1.0.0').set(1)
 
-HELLO_COUNTER = Counter('hellos', 'Total number of hellos')
-HELLO_FAILED_COUNTER = Counter('hellos_failed', 'Total number of failed hellos')
+HTTP_COUNTER = Counter('http_requests', 'Total number of HTTP requests per status code', ['code'])
 
-LATENCY = Histogram('hellos_latency_seconds', 'Histogram of latency',
+LATENCY = Histogram('hellos_latency_seconds', 'Histogram of latency for hello requests',
         buckets=(.0001, .0002, .0003, .0004, .0005))
 
 @app.route('/')
 def hello():
-    HELLO_COUNTER.inc()
     with LATENCY.time():
         try:
             name = request.args["name"]
             return f'Hello, {escape(name)}!'
         except:
-            HELLO_FAILED_COUNTER.inc()
             abort(500)
+
+@app.after_request
+def after_request_func(response):
+    HTTP_COUNTER.labels(code=response.status_code).inc()
+    return response
 
 app_dispatch = DispatcherMiddleware(app, {
     '/metrics': make_wsgi_app()
